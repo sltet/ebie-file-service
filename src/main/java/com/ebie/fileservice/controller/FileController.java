d1276c2747317ff10311a491e4640ae0371b8f8a
Steve Landry Tene <steve.tene@gmail.com>
2022-12-21 19:27:56 +0100

    adding zipping logic

--- src/main/java/com/ebie/fileservice/controller/FileController.java
+++ src/main/java/com/ebie/fileservice/controller/FileController.java
@@ -1,2 +1,42 @@
-package com.ebie.fileservice.controller;public class FileController {
+package com.ebie.fileservice.controller;
+
+import com.ebie.fileservice.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/file")
+public class FileController {
+
+	private final FileService fileService;
+
+	@PostMapping(
+			value = "/zip",
+			produces = "application/zip",
+			consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+	)
+	public ResponseEntity<StreamingResponseBody> zipFile(@RequestParam("file") MultipartFile file) {
+		String finalName = FilenameUtils.removeExtension(file.getOriginalFilename()) +".zip";
+		return ResponseEntity
+				.ok()
+				.header("Content-Disposition", "attachment; filename=" + finalName)
+				.body(out -> {
+					var zipOutputStream = new ZipOutputStream(out);
+					fileService.zipFile(zipOutputStream, file);
+				});
+	}
 }
--- src/main/java/com/ebie/fileservice/service/FileService.java
+++ src/main/java/com/ebie/fileservice/service/FileService.java
@@ -1,2 +1,24 @@
-package com.ebie.fileservice.service;public class FileService {
+package com.ebie.fileservice.service;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+@Service
+public class FileService {
+
+	public void zipFile(ZipOutputStream zipOutputStream, MultipartFile file) throws IOException {
+		zipOutputStream.putNextEntry(new ZipEntry(Objects.requireNonNull(file.getOriginalFilename())));
+		InputStream inputStream = file.getInputStream();
+		IOUtils.copy(inputStream, zipOutputStream);
+		inputStream.close();
+		zipOutputStream.closeEntry();
+		zipOutputStream.close();
+	}
 }
